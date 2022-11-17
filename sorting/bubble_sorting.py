a5a3f10d285bbd1c2facaee18a630a072dd35436
Thiago Vieira <thiagothiago9@hotmail.com>
2022-11-17 19:53:18 -0300

    bubble sorting

--- sorting/bubble_sorting.py
+++ sorting/bubble_sorting.py
@@ -2,11 +2,9 @@ def bubble_sorting(string):
 
     string = string.upper().replace(' ','')
     length = len(string)
-
     string_list = list(string)
 
-    for i in range(length):
-        
+    for i in range(length):        
         for j in range(length-1):
             if string_list[j] > string_list[j+1]:
                 aux = string_list[j+1]
@@ -17,6 +15,4 @@ def bubble_sorting(string):
 
 str_input = input()
 
-test = bubble_sorting(str_input)
-
-print(f'início: {test}')
+print(bubble_sorting(str_input))
